f82bf950cbe5a88e8e45362cc95f747871ee444a
Mahmoud Hosni <159634065+MahmoudHosni2000@users.noreply.github.com>
2024-05-13 07:23:01 +0300

    Update Form.jsx

--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -66,7 +66,7 @@ export default function Form() {
 
   const onSubmit = (data) => {
     const templateParams = {
-      to_name: "CodeBucks",
+      to_name: "Hosni",
       from_name: data.name,
       reply_to: data.email,
       message: data.message,
